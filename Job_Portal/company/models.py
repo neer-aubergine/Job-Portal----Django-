f20f13932593aac89b6f48958b6c83eb19c1a5e4
neer-aubergine <neer@auberginesolutions.com>
2024-12-29 15:39:03 +0530

    created a layout template & added Company model

--- Job_Portal/company/models.py
+++ Job_Portal/company/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Company(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    address = models.TextField(max_length=240)
+    contact = models.CharField(max_length=10)
+    created_At = models.DateTimeField(auto_now_add=True)
+    updated_At = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
