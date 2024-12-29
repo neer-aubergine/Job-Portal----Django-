ce19e69b85812b5773260e544b3df216f018d213
neer-aubergine <neer@auberginesolutions.com>
2024-12-29 21:12:39 +0530

    created views for company

--- Job_Portal/company/views.py
+++ Job_Portal/company/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
-
+from .models import Company
+from .forms import CompanyForm
+from django.shortcuts import get_object_or_404 , redirect
 # Create your views here.
 def index(request):
     return render(request , 'index.html')
+
+def company_list(request):
+    companies = Company.objects.all().order_by("-created_At")
+    return render(request , 'company_list.html' , {'companies' : companies})
+
+def company_create(request):
+    if request.method == "POST":
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            company = form.save(commit=False)
+            company.user = request.user
+            company.save()
+            return redirect('company_list')
+    else:
+        form = CompanyForm()
+    return render(request , 'company_form.html' , {'form' : form})
+
+def company_edit(request , company_id):
+    company = get_object_or_404(Company , pk = company_id , user = request.user)
+    if request.method == "POST":
+        form = CompanyForm(request.POST , instance=company)
+        if form.is_valid():
+            company = form.save(commit=False)
+            company.user = request.user
+            company.save()
+            return redirect("company_list")
+    else:
+        form = CompanyForm(instance=company)
+    return render(request , 'company_form.html' , {'form' : form})
+
+def company_delete(request , company_id):
+    company = get_object_or_404(Company , pk = company_id , user = request.user)
+    if request.method == "POST":
+        company.delete()
+        return redirect("company_list")
+    return render(request , 'company_confirm_delete.html' , {'company' : company})
