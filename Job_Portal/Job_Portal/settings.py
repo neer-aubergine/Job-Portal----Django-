3524f45859c8c220df0cfceae846773e42ec5e7b
neer-aubergine <neer@auberginesolutions.com>
2024-12-31 10:28:24 +0530

    added vercel.json

--- Job_Portal/Job_Portal/settings.py
+++ Job_Portal/Job_Portal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z^(#^ydqi^th83b%)fif!#kdm6fo*%fo(6fgnadfz(nzb_k1^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
